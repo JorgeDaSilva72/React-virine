12021e6148da1ab1aee4c3559c90044c541ab9a8
Jorge Da Silva <jorge.dasilva1@outlook.fr>
2020-11-22 17:49:43 +0100

    Creation du component SocialNetwork

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import SocialNetwork from "../components/SocialNetwork";
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
   return (
     <div>
 		<Navigation />
+		<SocialNetwork />
 	</div>
   );
 };

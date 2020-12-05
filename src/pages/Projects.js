b85425db1b62901c0f64bda3462d972666c11504
Jorge Da Silva <jorge.dasilva1@outlook.fr>
2020-12-05 16:04:39 +0100

    integration du component Project dans les pages projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Navigation from '../components/Navigation';
 import ButtonsBottom from '../components/ButtonsBottom';
 import Logo from '../components/Logo';
-
-
-
+import Project from '../components/Project';
 
 
 export const Project1 = () => {
@@ -14,10 +12,7 @@ export const Project1 = () => {
       <div className="project">
         <Navigation />
         <Logo />
-
-		Projet 1
-       
-        
+        <Project projectNumber={0} />
         <ButtonsBottom left={'/'} right={'/projet-2'} />
       </div>
     </main>
@@ -31,9 +26,7 @@ export const Project2 = () => {
       <div className="project">
         <Navigation />
         <Logo />
-
-        Projet 2
-        
+        <Project projectNumber={1} />
         <ButtonsBottom left={"/projet-1"} right={"/projet-3"} />
       </div>
     </main>
@@ -47,10 +40,7 @@ export const Project3 = () => {
       <div className="project">
         <Navigation />
         <Logo />
-
-		Projet 3
-        
-        
+        <Project projectNumber={2} />
         <ButtonsBottom left={"/projet-2"} right={"/projet-4"} />
       </div>
     </main>
@@ -64,10 +54,7 @@ export const Project4 = () => {
       <div className="project">
         <Navigation />
         <Logo />
-
-		Projet 4
-        
-        
+        <Project projectNumber={3} />
         <ButtonsBottom left={"/projet-3"} right={"/contact"} />
       </div>
     </main>

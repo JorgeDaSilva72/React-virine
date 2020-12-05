6363f565f6c85b5d48a9ee23f0449acef38aa5e8
Jorge Da Silva <jorge.dasilva1@outlook.fr>
2020-12-05 12:58:35 +0100

    integration du component dynamix text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,28 @@
 import React from "react";
 import Navigation from "../components/Navigation";
 import SocialNetwork from "../components/SocialNetwork";
+import DynamicText from "../components/DynamicText";
 
 
 const Home = () => {
 
 
   return (
-    <div>
-		<Navigation />
-		<SocialNetwork />
-	</div>
+	<main>
+		<div className="home">
+
+			<Navigation />
+			<SocialNetwork />
+			<div className="home-main">
+			  <div className="main-content">
+				<h1> WEB AGENCY </h1>
+				<h2> <DynamicText /> </h2>
+				  
+				
+			  </div>
+			</div>
+		</div>
+	</main>
   );
 };
 
